ec5a44bd6aedd0a203ac86964f5394069e10c497
ohlavrac <joaovicrcarvalho@gmail.com>
2024-03-14 12:23:41 -0300

    feat: add vehicle entity

---
+++ lib/domain/entities/vehicle_entity.dart
@@ -0,0 +1,28 @@
+class VehicleEntity {
+  int? id;
+  String? type;
+  String? name;
+  String? manufactor;
+  String? image;
+  int? sellprice;
+  int? topspeed;
+  int? seats;
+  String? drivetrain;
+  int? gears;
+  String? transmition;
+  int? storage;
+
+  VehicleEntity(
+      {this.id,
+      this.type,
+      this.name,
+      this.manufactor,
+      this.image,
+      this.sellprice,
+      this.topspeed,
+      this.seats,
+      this.drivetrain,
+      this.gears,
+      this.transmition,
+      this.storage});
+}
