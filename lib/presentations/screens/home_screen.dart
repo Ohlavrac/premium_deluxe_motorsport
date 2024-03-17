cf18705739d9ed82b272427bf208a01b6c963eed
ohlavrac <joaovicrcarvalho@gmail.com>
2024-03-16 22:50:14 -0300

    fix

--- lib/presentations/screens/home_screen.dart
+++ lib/presentations/screens/home_screen.dart
@@ -13,7 +13,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final _controller = PageController();
 
   @override
   Widget build(BuildContext context) {
@@ -27,113 +26,92 @@ class _HomeScreenState extends State<HomeScreen> {
         elevation: 0,
         title: const Text("Premium Deluxe Motorsport", style: TextStyle(color: AppColors.primary, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 200,
-                  child: Expanded(
-                    child: PageView.builder(
-                      controller: _controller,
-                      itemCount: 3,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          height: 300,
-                          child: Image.asset("./assets/ads/ad_oliver.png",),
-                        );
-                      },
-                    ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        child: Center(
+          child: Column(
+            children: [
+              const Text("PROCURE E COMPRE", style: TextStyle(color: AppColors.primary, fontWeight: FontWeight.bold, fontSize: 18),),
+              const Text("OS MELHORES CARROS DE LOS SANTOS COM OS MELHORES PREÇOS.", style: TextStyle(color: AppColors.primary, fontWeight: FontWeight.w500, fontSize: 14),),
+              const Divider(color: Colors.blue, indent: 10, endIndent: 10, thickness: 2,),
+              const Divider(color: AppColors.primary, indent: 10, endIndent: 10, thickness: 2,),
+              const Divider(color: Colors.red, indent: 10, endIndent: 10, thickness: 2,),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: 10,
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 400,
+                    childAspectRatio: 2 / 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10
                   ),
-                ),
-                const SizedBox(height: 20,),
-                const Text("PROCURE E COMPRE", style: TextStyle(color: AppColors.primary, fontWeight: FontWeight.bold, fontSize: 18),),
-                const Text("OS MELHORES CARROS DE LOS SANTOS COM OS MELHORES PREÇOS.", style: TextStyle(color: AppColors.primary, fontWeight: FontWeight.w500, fontSize: 14),),
-                const Divider(color: Colors.blue, indent: 10, endIndent: 10, thickness: 2,),
-                const Divider(color: AppColors.primary, indent: 10, endIndent: 10, thickness: 2,),
-                const Divider(color: Colors.red, indent: 10, endIndent: 10, thickness: 2,),
-                SizedBox(
-                  height: 1000,
-                  child: Expanded(
-                    child: GridView.builder(
-                      itemCount: 10,
-                      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                        maxCrossAxisExtent: 400,
-                        childAspectRatio: 2 / 2,
-                        crossAxisSpacing: 10,
-                        mainAxisSpacing: 10
+                  itemBuilder: (context, index) {
+                    return Card(
+                      color: AppColors.secondary,
+                      clipBehavior: Clip.hardEdge,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
                       ),
-                      itemBuilder: (context, index) {
-                        return Card(
-                          color: AppColors.secondary,
-                          clipBehavior: Clip.hardEdge,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5.0),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(width: double.infinity, child: Image.asset(vehicles[0].image!, fit: BoxFit.cover)),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
                             children: [
-                              SizedBox(width: double.infinity, child: Image.asset(vehicles[0].image!, fit: BoxFit.cover)),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                children: [
-                                  RichText(
-                                    text: TextSpan(
-                                      children: [
-                                        TextSpan(text: "${vehicles[0].manufactor} ", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
-                                        TextSpan(text: " ${vehicles[0].name}", style: TextStyle(color: Colors.black87)),
-                                      ]
-                                    ),
-                                  ),
-                                  Text("${vehicles[0].type}"),
-                                ],
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 10),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                              RichText(
+                                text: TextSpan(
                                   children: [
-                                    Text("Assentos: ${vehicles[0].seats}"),
-                                    Text("Tração: ${vehicles[0].drivetrain}"),
-                                    Text("Transmissão: ${vehicles[0].transmition} de ${vehicles[0].gears} marchas"),
-                                    Text("Porta-malas: ${vehicles[0].storage}L")
-                                  ],
+                                    TextSpan(text: "${vehicles[0].manufactor} ", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+                                    TextSpan(text: " ${vehicles[0].name}", style: TextStyle(color: Colors.black87)),
+                                  ]
                                 ),
                               ),
-                              const SizedBox(height: 10,),
-                              Center(
-                                child: ElevatedButton(
-                                  onPressed: () {
-                                    showDialog(context: context, builder: (builder) {
-                                      return const AlertDialog(
-                                        icon: Icon(Icons.call),
-                                        title: Text("Entre em contato"),
-                                        content: Text("Zaja: (XXX) XXX-XXXX\nAlexis: (XXX) XXX-XXXX\nOliver: (XXX) XXX-XXXX\nMaxine: (XXX) XXX-XXXX"),
-                                      );
-                                    });
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: AppColors.background,
-                                    elevation: 0,
-                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0))
-                                  ), 
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(horizontal: 100),
-                                    child: Text("${vehicles[0].sellprice}\$", style: const TextStyle(color: AppColors.primary, fontWeight: FontWeight.bold),),
-                                  ),
-                                )
-                              ),
+                              Text("${vehicles[0].type}"),
                             ],
-                          )
-                        );
-                      }
-                    ),
-                  ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Assentos: ${vehicles[0].seats}"),
+                                Text("Tração: ${vehicles[0].drivetrain}"),
+                                Text("Transmissão: ${vehicles[0].transmition} de ${vehicles[0].gears} marchas"),
+                                Text("Porta-malas: ${vehicles[0].storage}L")
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 10,),
+                          Center(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                showDialog(context: context, builder: (builder) {
+                                  return const AlertDialog(
+                                    icon: Icon(Icons.call),
+                                    title: Text("Entre em contato"),
+                                    content: Text("Zaja: (XXX) XXX-XXXX\nAlexis: (XXX) XXX-XXXX\nOliver: (XXX) XXX-XXXX\nMaxine: (XXX) XXX-XXXX"),
+                                  );
+                                });
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: AppColors.background,
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0))
+                              ), 
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 100),
+                                child: Text("${vehicles[0].sellprice}\$", style: const TextStyle(color: AppColors.primary, fontWeight: FontWeight.bold),),
+                              ),
+                            )
+                          ),
+                        ],
+                      )
+                    );
+                  }
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
