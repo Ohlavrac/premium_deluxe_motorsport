5bca513565461f347c074c25d35909bcfb4b25e7
ohlavrac <joaovicrcarvalho@gmail.com>
2024-03-16 22:46:40 -0300

    update

---
+++ lib/presentations/shared/colors/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color.fromARGB(255, 41, 41, 41);
+  static const primary = Color(0xfff1f1f1);
+  static const secondary = Color.fromARGB(255, 212, 212, 212);
+}
